a94f8c120b737702aa45dc0f5e6d1b6f87e03b05
SanchitMittalG <sanchitmittal270306@gmail.com>
2025-05-27 23:02:20 +0530

    final commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer/>
+    </div>
+  )
 }
